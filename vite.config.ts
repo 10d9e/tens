d59ccfa8544c9383e60f347206a8efdee9223a6f
lodge <jay.logelin@gmail.com>
2025-10-09 12:16:57 -0300

    bump

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        host: '0.0.0.0', // Allow access from any IP on the network
+        host: '0.0.0.0', // Allow access from any IP addr on the network
         port: 3000,
         proxy: {
             '/socket.io': {
